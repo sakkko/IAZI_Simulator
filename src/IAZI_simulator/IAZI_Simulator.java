7426e6a6a5a916f7208b266ed3c1ed88fbcd0fff
Simone Sagone <il_sacko@hotmail.it>
2011-11-14 20:06:59 +0100

    Aggiunto run statistici: calcolo tempo medio di risposta.

--- src/IAZI_simulator/IAZI_Simulator.java
+++ src/IAZI_simulator/IAZI_Simulator.java
@@ -19,7 +19,7 @@ public class IAZI_Simulator {
 	 */
 	public static void main(String args[]){		
 		IAZI_Simulator iazi = new IAZI_Simulator();
-		int n0 = 0;
+		int n0 = 0;		
 		
 		try {
 			n0 = iazi.runStabilizzazione();
@@ -31,7 +31,130 @@ public class IAZI_Simulator {
 		
 	}
 	
-	public void runStatistici(int n0) {
+	public void runStatistici(int n0) throws GeneratoreException, EventoException, CentroException {
+		int p = 100;
+		int[] lunghezza_run = new int[p];
+		Generatore gen = null;
+		long seme = 777777777L;
+		int sup = 100, inf = 50;
+		Impianto imp;
+		Scheduler sched;
+		long[] semiGeneratori;
+		int n;
+		int count;
+		double yj, zj;
+		double[] vettore_yj = new double[p];
+		double media_yj, media_zj, media_nj;
+		double valore_medio;
+		double u_alpha_mezzi = 1.96;
+		double s11_quadro, s12_quadro, s22_quadro, s_quadro;
+		double d;
+		double inf_intervallo_confidenza;			
+		double sup_intervallo_confidenza;
+		Job job = null;
+		double next_time;
+		
+		for (int i = 1; i <= 12; i ++) {
+			gen = new GeneratoreUniforme(seme);
+			for (int j = 0; j < p; j ++) {
+				lunghezza_run[j] = (int)(gen.getNext() * sup);
+				if (lunghezza_run[j] < inf) {
+					lunghezza_run[j] += inf;
+				}
+			}
+			
+			media_yj = 0.0;
+			media_zj = 0.0;
+			media_nj = 0.0;
+			
+			for (int j = 0; j < p; j ++) {
+				semiGeneratori = getSemiIniziali(seme);
+				//TODO controllare
+				seme = semiGeneratori[semiGeneratori.length - 1];
+				
+				imp = new Impianto(semiGeneratori, 12, 3);
+				sched = new Scheduler(imp);
+				
+				for (int k = 0; k < i; k ++) {
+					job = new Job();
+					next_time = imp.getClientPC().get(job.getId()).aggiungiJob(job);
+					sched.getCalendario().aggiungiEvento(new FinePcHC("fine_pchc", next_time, job.getId()));
+				}
+				
+				//STABILIZZAZIONE
+				n = 0;				
+				while (n < n0) {
+					sched.schedule();
+					if (n < FineLAN1.getJobTerminati()) {
+						n = FineLAN1.getJobTerminati();
+					}
+				} //IMPIANTO STABILIZZATO
+				
+				yj = 0.0;
+				zj = 0.0;
+				count = 0;
+				
+				while (count < lunghezza_run[j]) {
+					sched.schedule();
+					if (n < FineLAN1.getJobTerminati()) {
+						yj += FineLAN1.getTempoRispostaJob();
+						zj += Math.pow(FineLAN1.getTempoRispostaJob(), 2);
+						n = FineLAN1.getJobTerminati();
+						count ++;
+					}
+				}
+				
+				vettore_yj[j] = yj;
+				media_yj += yj;
+				media_zj += zj;							
+			} //FINITI TUTTI I p RUN DI LUNGHEZZA n VARIABILE
+			
+			media_yj /= p;
+			media_zj /= p;
+			
+			for (int k = 0; k < p; k ++) {
+				media_nj += lunghezza_run[k];
+			}
+			
+			media_nj /= p;
+			
+			//E(x)
+			valore_medio = media_yj / media_nj;
+			
+			s11_quadro = 0.0;
+			for (int k = 0; k < p; k ++) {
+				s11_quadro += Math.pow(vettore_yj[k] - media_yj, 2);
+			}
+			
+			s11_quadro /= (p - 1);
+			
+			s22_quadro = 0.0;
+			for (int k = 0; k < p; k ++) {
+				s22_quadro += Math.pow(lunghezza_run[k] - media_nj, 2);
+			}
+			
+			s22_quadro /= (p - 1);
+			
+			s12_quadro = 0.0;
+			for (int k = 0; k < p; k ++) {
+				s12_quadro += (lunghezza_run[k] - media_nj) * (vettore_yj[k] - media_yj);
+			}
+			
+			s12_quadro /= (p - 1);
+			
+			s_quadro = s11_quadro - 2 * valore_medio * s12_quadro + Math.pow(valore_medio, 2) * s22_quadro;
+			
+			d = Math.sqrt(s_quadro) / (media_nj * Math.sqrt(p));
+			
+			inf_intervallo_confidenza = valore_medio - (d * u_alpha_mezzi);			
+			sup_intervallo_confidenza = valore_medio + (d * u_alpha_mezzi);
+			
+			System.out.println("Valor Medio " + valore_medio + " di N = " + i + " job nell'impianto");
+			System.out.println("estremo inf intervallo di confidenza al 90%  " + inf_intervallo_confidenza);
+			System.out.println("estremo sup intervallo di confidenza al 90%  " + sup_intervallo_confidenza);
+			
+		}
+		
 		
 	}
 	
@@ -59,7 +182,7 @@ public class IAZI_Simulator {
 			for (int k = 0; k < p; k ++) {
 				imp = new Impianto(semiGeneratori, 12, 3);
 				sched = new Scheduler(imp);						
-				caricaJob(imp, sched);
+				caricaJob(imp, sched, imp.getnClient());
 				
 				jobTerminati = 0;
 				tempoMedioRisposta = 0.0;
@@ -146,11 +269,34 @@ public class IAZI_Simulator {
 		return semi;
 	}
 	
-	public void caricaJob(Impianto imp, Scheduler sched) throws EventoException {
+	public long[] getSemiIniziali(long num) {
+		Generatore gen = null;
+		long []semi = new long[153];
+		int i = 0;
+		
+		while (i < 153) {
+			gen = new GeneratoreUniforme(num);
+			if (TestGeneratoreUniforme.ChiQuadroTest(gen)) {
+				semi[i] = num;				
+				//System.out.println("Seme " + i + ": " + num);
+				i ++;
+			} 
+			num = (long)(gen.getNext() * Integer.MAX_VALUE);
+			
+			if (num % 2 == 0) {
+				//il seme deve essere dispari	
+				num ++;
+			}			
+		}
+		
+		return semi;
+	}
+	
+	public void caricaJob(Impianto imp, Scheduler sched, int nJob) throws EventoException {
 		Job job;
 		double next_time;
 		
-		for (int j = 0; j < 12; j ++) {
+		for (int j = 0; j < nJob; j ++) {
 			job = new Job();
 			next_time = imp.getClientPC().get(j).aggiungiJob(job);
 			sched.getCalendario().aggiungiEvento(new FinePcHC("fine_pchc", next_time, j));
